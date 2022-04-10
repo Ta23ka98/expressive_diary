74185b14f3fe742b318b9a533bf76df8b6d133a8
Ta23ka98 <shinonome419@icloud.com>
2022-04-11 07:23:09 +0900

    8th commit. Added dateTime and form into AddEventScreen, and arranged their styles.

--- lib/Screens/add_event_screen.dart
+++ lib/Screens/add_event_screen.dart
@@ -8,14 +8,34 @@ class AddEventScreen extends StatefulWidget {
 }
 
 class _AddEventScreenState extends State<AddEventScreen> {
+  DateTime _focusedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AddEventScreen"),
+        title: const Text("AddEventScreen"),
       ),
-      body: Column(
-        children: [],
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "${_focusedDay.month.toString()}/${_focusedDay.day.toString()}",
+              style: TextStyle(fontSize: 35),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  border: Border.all(color: Colors.blue, width: 1)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
